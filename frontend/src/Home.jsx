5356b838ab63f4acfe0aba3b48bd635b70d6ffb6
youssef <youssefmoussaddeq00@gmail.com>
2025-11-20 13:50:47 +0100

    added half home page and acroll animation

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,16 +3,16 @@ import "./home.css";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-    const [animateText, setAnimate] = useState(false)
-    const [animateImg, setAnimateImg] = useState(false)
-    useEffect(()=>{
-        setTimeout(()=>{
-            setAnimate(true)
-        },1000)
-        setTimeout(()=>{
-            setAnimateImg(true)
-        },1000)
-    },[])
+  const [animateText, setAnimate] = useState(false);
+  const [animateImg, setAnimateImg] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimate(true);
+    }, 1000);
+    setTimeout(() => {
+      setAnimateImg(true);
+    }, 1000);
+  }, []);
   return (
     <div className="homeContainer">
       <header>
@@ -34,7 +34,13 @@ export default function Home() {
         </nav>
       </header>
       <main>
-        <div className={`marketingText ${!animateText?"display":""}`} style={{display:`${animateText?"block":"none"}}`,right:!animateText?1000:0}}>
+        <div
+          className={`marketingText ${!animateText ? "display" : ""}`}
+          style={{
+            display: `${animateText ? "block" : "none"}}`,
+            right: !animateText ? 1000 : 0,
+          }}
+        >
           <h1>
             Discover the best deals on the products you love. Shop <b>smart</b>,
             shop <b>fast</b>
@@ -43,13 +49,46 @@ export default function Home() {
             <span>shop now</span>
           </button>
         </div>
-        <article className={`${!animateImg?"animateImg":""}`}>
-            <img
+        <article className={`${!animateImg ? "animateImg" : ""}`}>
+          <small>
+            Find everything you need in one place. Simple, fast, reliable
+          </small>
+          <img
+            className="puffer"
             src="puffer.gif"
-            style={{ overflow: "hidden",}}
-            />
+            style={{ overflow: "hidden" }}
+          />
+          <img
+            src="products_image-removebg-preview (2).png"
+            className="productsImage"
+          />
         </article>
       </main>
+      <section>
+        <h1 className="textFlowt">COMFORT</h1>
+        <div className="forLeft">
+          <img
+            src="vue-rapprochee-d-une-personne-portant-des-baskets-futuristes.jpg"
+            alt=""
+          />
+          <small>
+            Shop smarter with premium products, affordable deals, and quick
+            delivery.
+          </small>
+        </div>
+        <div className="forMiddle">
+          <img
+            src="gros-plan-sur-un-homme-devant-des-piles-de-vetements.jpg"
+            alt=""
+          />
+        </div>
+        <div className="forRight">
+          <div className="rightP">
+            <small>Simple, stylish, and affordable. Your favorite products, just a click away.</small>
+          </div>
+          <img src="f7af44fe62017af88857e939d7550b9f.jpg" alt="" />
+        </div>
+      </section>
     </div>
   );
 }
