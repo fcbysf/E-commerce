afd3a4dafd33927f70b0b2ba5991763399fda28d
youssef <youssefmoussaddeq00@gmail.com>
2026-01-04 16:17:52 +0100

    back

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        $this->call(DemoUserSeeder::class);
 
         User::factory()->create([
             'name' => 'Test User',
